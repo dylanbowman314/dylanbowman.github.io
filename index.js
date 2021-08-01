1187e71c8bf9c865aeec0bb2f596da0b7096ceb9
dylanbowman314 <55146842+dylanbowman314@users.noreply.github.com>
2021-07-31 23:43:03 -0500

    Creates scene2

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import * as d3 from "https://cdn.skypack.dev/d3@7";
 
-const margin = {top: 100, right: 10, bottom: 20, left: 500};
-const width = 1000 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+const margin = {top: 10, right: 10, bottom: 20, left: 10};
+const barChart = {x: 900, y: 100, barHeight: 100}
+const width = 1400 - margin.left - margin.right,
+    height = 900 - margin.top - margin.bottom;
+const globalColors = {default:"steelblue", correct:"green", incorrect:"red", continue:"yellow"}
 let pointRadius = 5
 
 
@@ -10,7 +12,7 @@ const g = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform", "translate(" + (margin.left + 100) + "," + margin.top + ")");
+    .attr("transform", "translate(" + (margin.left + barChart.x) + "," + (margin.top + barChart.y) + ")");
 
 
 function drawScatterplot(xfield, yfield, pointColor,source) {
@@ -89,19 +91,48 @@ function drawBarGraph(xfield, barColor, barHeight, barWidthFactor) {
                 .attr("height", barHeight - 3)
                 .style("fill","white")
                 .transition().duration(1500)
-                .attr("width", function(d) {return (d[xfield] * barWidthFactor/width)})
+                .attr("width", function(d) {return (d[xfield] * barWidthFactor)})
                 .attr("height", barHeight - 3)
                 .style("fill",barColor);
 
-            g.selectAll("rect").append("text")
-                .attr("x", function(d) { return 10; })
-                .attr("y", barHeight / 2)
-                .attr("dy", ".35em")
-                .text(function(d) { return d["country"]; });
+            var texts = g.selectAll(".myTexts")
+                .data(data)
+                .enter()
+                .append("text");
 
+            texts
+                .attr("y", function(d,i){ return i*barHeight + 0.5 * barHeight})
+                .transition().duration(1500)
+                .attr("x", function(d){ return (d[xfield] * barWidthFactor + 10)})
+                .attr("y", function(d,i){ return i*barHeight + 0.5 * barHeight})
+                .text(function(d){ return d["vaccines_safe"]+"%"});
 
+            d3.select("rect").style("color","lime")
 
         });
+}
+
+function drawButton (x,y,width,height,text,startColor,endColor,func) {
+    d3.select("svg").append("rect")
+        .attr("x",x)
+        .attr("y",y)
+        .attr("width",width)
+        .attr("height",height)
+        .style("fill",startColor)
+        .style("stroke","black")
+        .style("stroke-width",2)
+        .on("click",function() {
+            d3.select(this).style("fill",endColor)
+            func()
+        })
+
+    d3.select("svg").append("text")
+        .attr("x",x + width/2)
+        .attr("y",y + height/2)
+        .text(text)
+        .style("text-anchor", "middle")
+        .style('font-size', '36px')
+        .style('font-family', '"Open Sans", sans-serif')
 }
 
 function countryFilter(countryName) {
@@ -111,8 +142,54 @@ function countryFilter(countryName) {
 }
 
 function clearSVG() {
-    d3.selectAll("svg > *").remove();
+    g.selectAll("*").remove()
+    d3.select("svg").selectAll("rect").remove()
+    d3.select("svg").selectAll("circle").remove()
+    d3.select("svg").selectAll("text").remove()
 }
 
-drawBarGraph("vaccines_safe","steelblue",100,2000)
+//drawBarGraph("vaccines_safe","steelblue",100,2000)
 
+let buttonX = (width - barChart.x - margin.left) - 10 // for gap
+let buttonY = barChart.y + margin.top
+let buttonWidth = -2 * (width / 2 - (barChart.x + margin.left)) - 10
+
+
+function setScene2() {
+    clearSVG()
+    let countries = ["Bangladesh", "Japan", "Libya", "USA"]
+    let colors = [globalColors.correct, globalColors.incorrect, globalColors.incorrect, globalColors.incorrect]
+
+    d3.select("svg").append("text")
+        .attr("x",buttonX + buttonWidth/2)
+        .attr("y",buttonY - 50)
+        .text("Which of these countries has the highest trust in vaccines?")
+        .style("text-anchor", "middle")
+        .style('font-size', '36px')
+        .style('font-family', '"Open Sans", sans-serif')
+        .style("font-weight","bold")
+
+    for (let i = 0; i < 4; i++) {
+        drawButton(buttonX,buttonY + i * barChart.barHeight,buttonWidth,barChart.barHeight - 10,countries[i],
+            "gray",colors[i],
+            function() {
+            drawBarGraph("vaccines_safe","steelblue",barChart.barHeight,4);
+            drawButton(buttonX,buttonY + 4*barChart.barHeight,buttonWidth,barChart.barHeight,
+                "Continue",globalColors.continue,globalColors.continue,function(){setScene3()})
+        })
+    }
+}
+
+function setScene3() {
+    clearSVG()
+    d3.select("svg").append("text")
+        .attr("x",buttonX + buttonWidth/2)
+        .attr("y",buttonY - 50)
+        .text("hi matthew")
+        .style("text-anchor", "middle")
+        .style('font-size', '36px')
+        .style('font-family', '"Open Sans", sans-serif')
+        .style("font-weight","bold")
+}
+
+setScene2()
