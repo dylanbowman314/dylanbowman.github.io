79d03e5658213329731dc34f651dd8033b4291a6
dylanbowman314 <55146842+dylanbowman314@users.noreply.github.com>
2021-07-31 22:08:01 -0500

    Creates bar graph.

--- index.js
+++ index.js
@@ -1,46 +1,118 @@
 import * as d3 from "https://cdn.skypack.dev/d3@7";
 
-var vax_data;
-var svgWidth = 500, svgHeight = 500, pointRadius = 3;
+const margin = {top: 100, right: 10, bottom: 20, left: 500};
+const width = 1000 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+let pointRadius = 5
 
-d3.csv("https://raw.githubusercontent.com/irinavrt/vaccineconf-by-religiosity/main/vaccineconf-and-religiosity-combined.csv")
-    .then(function(data) {
-        var svg = d3.select("svg")
-            .attr("width",svgWidth)
-            .attr("height",svgHeight)
 
-        var points = svg.selectAll("circle")
-            .data(data)
-            .enter()
-            .append("circle")
-            .attr("cx",function(d) {return d["religiosity"]*5})
-            .attr("cy",function(d) {return svgHeight - d["vaccines_safe"]*5})
-            .attr("r",pointRadius)
-            .style("fill","steelblue")
+const g = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + (margin.left + 100) + "," + margin.top + ")");
 
-        var scale = d3.scaleLinear().domain([0,svgWidth]).range([0,svgHeight])
 
-        var xAxis = d3.axisBottom(scale)
-        var yAxis = d3.axisLeft(scale)
+function drawScatterplot(xfield, yfield, pointColor,source) {
+    d3.csv(source)
+        .then(function(data) {
+            let maxX = (xfield.includes("hdi") ? 1 : 100)
+            let maxY = (yfield.includes("hdi") ? 1 : 100)
 
-        svg.append("g").call(xAxis).call(yAxis)
-    });
+            let points = g.selectAll("circle")
+                .data(data)
+                .enter()
+                .append("circle")
+                .attr("cx",function(d) {return d[xfield]*width/maxX + margin.left})
+                .attr("cy",function(d) {return height + margin.top - d[yfield]*height/maxY - 15})
+                .attr("r",pointRadius)
+                .style("fill",pointColor)
 
-/*setTimeout(function() {
-    console.log(vax_data);
+            let xScale = d3.scaleLinear()
+                .domain([0,maxX])
+                .range([0, width]);
+            let xAxisGenerator = d3.axisBottom(xScale);
+            let xAxis =  g.append("g")
+                .call(xAxisGenerator)
+                .attr("transform",`translate(${0},${height - 20})`)
 
-    const container = d3.selectAll("svg")
-        .classed("container",true)
+            let yScale = d3.scaleLinear()
+                .domain([0,maxY])
+                .range([height - margin.top, 20]);
+            let yAxisGenerator = d3.axisLeft(yScale);
+            let yAxis =  g.append("g")
+                .call(yAxisGenerator)
+                .attr("transform",`translate(${margin.left + 20},${margin.top})`)
 
-    const bars = container
-        .selectAll(".bar")
-        .data(vax_data)
-        .enter()
-        .append("rect")
-        .classed("bar",true)
-        .attr("width",50)
-        .attr("height",20)
-        .fill("lime")
+            g.append("text")
+                .attr("y", margin.top + height - 20)
+                .attr("x", margin.left + width/2)
+                .attr("dy", "1em")
+                .style("text-anchor", "middle")
+                .text(xfield);
 
-},400);*/
+            g.append("text")
+                .attr("class","y_axis_label")
+                .attr("y", 400)
+                .attr("x", 400)
+                .attr("transform","rotate(270)")
+                .attr("dy", "1em")
+                .attr("dx", "-1em")
+                .style("text-anchor", "middle")
+                .text(yfield);
+
+
+            // turn red when clicked
+            g.selectAll("circle")
+                .on("mouseover",function(e,d) {
+                    d3.selectAll("circle").style("fill",pointColor)
+                    d3.select(this).style("fill","red")
+                    d3.select(this).raise()
+                })
+                .on("click",function(e,d) {
+                    d3.selectAll("circle").style("fill",pointColor)
+                    d3.select(this).style("fill","red")
+                    d3.select(this).raise()
+                })
+        });
+}
+
+function drawBarGraph(xfield, barColor, barHeight, barWidthFactor) {
+    d3.csv("resources/bardata.csv")
+        .then(function(data) {
+            let bar = g.selectAll("g")
+                .data(data)
+                .enter().append("rect")
+                .classed("bar",true)
+                .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; })
+                .attr("width", 20)
+                .attr("height", barHeight - 3)
+                .style("fill","white")
+                .transition().duration(1500)
+                .attr("width", function(d) {return (d[xfield] * barWidthFactor/width)})
+                .attr("height", barHeight - 3)
+                .style("fill",barColor);
+
+            g.selectAll("rect").append("text")
+                .attr("x", function(d) { return 10; })
+                .attr("y", barHeight / 2)
+                .attr("dy", ".35em")
+                .text(function(d) { return d["country"]; });
+
+
+
+        });
+}
+
+function countryFilter(countryName) {
+    g.selectAll("circle").remove()
+
+    g.selectAll("circle")
+}
+
+function clearSVG() {
+    d3.selectAll("svg > *").remove();
+}
+
+drawBarGraph("vaccines_safe","steelblue",100,2000)
 
